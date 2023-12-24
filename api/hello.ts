8ade96e257de013c7ba097b0d3abe7ddebe01b5c
Travis Wagner <trvswgnr@gmail.com>
2023-12-23 20:28:39 -0500

    Add import for add function and use it in hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { add } from '../lib';
 
 export default ({ query }: VercelRequest, res: VercelResponse) => {
-  const { name = 'World' } = query;
+  const { name = add(1, 3) } = query;
 
   return res.json({
     message: `Hello ${name}!`,
