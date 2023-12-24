b13ed0c442384a442e08f7166c672b2c2d577483
Travis Wagner <trvswgnr@gmail.com>
2023-12-23 20:29:17 -0500

    Update import statement in hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { add } from '../lib';
+import { add } from '../lib.ts';
 
 export default ({ query }: VercelRequest, res: VercelResponse) => {
   const { name = add(1, 3) } = query;
