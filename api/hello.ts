e9cf85c8d0e6094473d90190946581686c30bb84
Travis Wagner <trvswgnr@gmail.com>
2023-12-23 20:30:52 -0500

    update

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { add } from './lib.js';
+import { add } from './lib';
 
 export default ({ query }: VercelRequest, res: VercelResponse) => {
   const { name = add(1, 3) } = query;
